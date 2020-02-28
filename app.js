6faf6d9ae0c20b03f0968676b1974d6855ab2a67
BitNaKm <37769713+BitNaKm@users.noreply.github.com>
2020-02-29 01:06:06 +0900

    Update app.js
    자유게시판 완성

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', mainRouter);
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
-app.use('/board', boardRouter);
+app.use('/freeboard', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
