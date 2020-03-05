6430c1ed46c65de85719e0355ff1d23081dac4e5
BitNaKm <37769713+BitNaKm@users.noreply.github.com>
2020-03-05 14:01:33 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,5 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-//router.get('/user', function(req, res, next) {
-//    res.render('test_user');
-//});
-
-//router.get('/board', function(req, res, next) {
-//    res.render('test_board');//});
-
 module.exports = router;
 
